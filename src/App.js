91e47c3a6cd8309e6c5bab6e1e8f2478da96a0eb
ANURAGNAKUL2702 <anuragnakul2702@gmail.com>
2025-08-28 14:15:45 +0530

    Update amazing feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import DinoGame from './components/DinoGame'; // Correct import if DinoGame.js is directly under components
+import DinoGame from './components/DinoGame'; 
 import './App.css';
 
 function App() {
   const [showGame, setShowGame] = useState(false);
-  
+
   if (showGame) {
     return <DinoGame onExit={() => setShowGame(false)} />;
   }
@@ -12,17 +12,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Your App</h1>
-        
-        {/* Attractive Dino Game Button */}
-        <div className="game-button-container" onClick={() => setShowGame(true)}>
-          <div className="dino-icon">🦖</div>
-          <div className="button-content">
-            <span className="button-title">PLAY DINO GAME</span>
-            <span className="button-subtitle">Click to start the adventure!</span>
-          </div>
-          <div className="button-arrow">🎮</div>
-        </div>
+        <h1>Play your offline game</h1>
+
+        {/* Replace old container with single PLAY button */}
+        <button className="play-button" onClick={() => setShowGame(true)}>
+          CLICK HERE TO PLAY
+        </button>
       </header>
     </div>
   );
